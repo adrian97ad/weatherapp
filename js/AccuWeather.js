37d500018f01ba290f034f75bf0d154a216e1265
adralcan <adrian97ad@gmail.com>
2022-11-24 21:55:11 +0100

    Primera prueba técnica

---
+++ js/AccuWeather.js
@@ -0,0 +1,26 @@
+class AccuWeather {
+    constructor() {
+        this._apikey = '81yA6ycWU6qvQwagPZCAhjTA5ApKnnig';
+        this.language = 'es-ES';
+        this.basicURL = 'http://dataservice.accuweather.com/';
+    }
+    static getInstance() {
+        if(!AccuWeather.instance) {
+            this.instance = new AccuWeather();
+        }
+        return this.instance;
+    }
+
+    async getCity(cityName) {
+        const city = await fetch(`${this.basicURL}locations/v1/cities/search?apikey=${this._apikey}&q=${encodeURI(cityName)}&language=${this.language}&details=false&offset=5`);
+        const data = await city.json();
+        return data[0].Key;
+    }
+
+    async getWeatherCiy(cityName) {
+        const cityCode = await this.getCity(cityName);
+        const city = await fetch(`${this.basicURL}/currentconditions/v1/${cityCode}?apikey=${this._apikey}&language=${this.language}&details=true`);
+        const data = await city.json();
+        return data[0];
+    }
+}
